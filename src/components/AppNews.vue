8849dac4a69f3ffb2f9e5cbd64dbc7cd03629d64
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-10-30 15:35:32 +0500

    Add AppNews component

---
+++ src/components/AppNews.vue
@@ -0,0 +1,184 @@
+<template>
+  <section class="py-10 bg-blue-50">
+    <div class="container">
+      <!-- Header -->
+      <header class="flex items-center justify-between mb-8">
+        <h2 class="mb-4 text-3xl font-semibold text-gray-800">Yangiliklar</h2>
+        <a href="#" class="duration-500 text-primary-blue hover:opacity-80"
+          >Barchasini ko'rish</a
+        >
+      </header>
+
+      <!-- Main Content Grid -->
+      <div class="grid grid-cols-1 gap-6 lg:grid-cols-3">
+        <!-- Large Article -->
+        <div
+          class="relative overflow-hidden bg-white rounded-lg shadow-lg lg:col-span-2"
+        >
+          <img
+            src="https://tsue.uz/media/news/photo_2024-10-24_19-11-34.jpg"
+            alt="Main article image"
+            class="object-cover w-full h-64 lg:h-80"
+          />
+          <div class="h-full p-6 text-white bg-primary-blue">
+            <span
+              class="inline-block px-3 py-1 mb-3 text-xs font-bold uppercase rounded bg-primary-blue"
+              >ВАЖНО</span
+            >
+            <p class="text-xs opacity-75">29.10.2024</p>
+            <h2 class="mt-1 text-2xl font-bold">
+              Учёные открыли семейство бактерий, способных дышать кислородом под
+              землей
+            </h2>
+            <p class="mt-2 text-sm opacity-75">
+              «Поймать» ранее неизвестные бактерии удалось с помощью
+              метагеномного анализа.
+            </p>
+            <a
+              href="#"
+              class="inline-block mt-4 font-semibold underline text-primary-blue"
+              >Подробнее</a
+            >
+          </div>
+        </div>
+
+        <!-- Small Articles -->
+        <div class="flex flex-col gap-4 judstify-between">
+          <div
+            v-for="article in articles"
+            :key="article.id"
+            class="h-full overflow-hidden transform bg-white rounded-lg shadow-lg small-card"
+          >
+            <img
+              :src="article.image"
+              alt="Article image"
+              class="object-cover w-full h-32 transition-transform duration-500 hover:scale-105 small-card__img"
+            />
+            <div class="block p-4 small-card__content">
+              <div class="flex items-center justify-between mb-2">
+                <p class="text-xs text-gray-500">{{ article.date }}</p>
+                <span
+                  class="px-2 py-1 text-xs font-semibold text-white rounded"
+                  :class="getCategoryColor(article.category)"
+                >
+                  {{ article.category }}
+                </span>
+              </div>
+              <a
+                href="#"
+                class="mb-1 text-lg font-bold leading-snug text-primary-blue line-clamp-2"
+              >
+                {{ article.title }}
+              </a>
+              <p
+                class="h-0 transition-all duration-500 opacity-0 text-neutral-gray small-card__text shrink-0 line-clamp-4"
+              >
+                Далеко-далеко за словесными горами в стране гласных и согласных
+                живут рыбные тексты. То своих однажды путь семантика залетают
+                власти жизни буквенных. Буквенных эта пустился обеспечивает
+                использовало наш речью, бросил курсивных власти злых?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Additional Sections Below -->
+      <div class="grid grid-cols-1 gap-6 mt-8 lg:grid-cols-2">
+        <!-- Rector's Message -->
+        <div
+          class="flex items-center p-6 transition-transform transform bg-white rounded-lg shadow-lg hover:scale-105"
+        >
+          <img
+            src="https://tsue.uz/media/news/photo_2024-10-24_19-11-34.jpg"
+            alt="Rector"
+            class="object-cover w-24 h-24 mr-4 rounded-lg"
+          />
+          <div>
+            <h2 class="text-xl font-bold text-primary-blue">Слово — ректору</h2>
+            <p class="mt-2 text-sm leading-snug text-gray-600">
+              Реформа высшего образования: чего ждут работодатели и к чему они
+              готовы?
+            </p>
+            <p class="mt-1 text-xs text-gray-500">30.09.2024</p>
+          </div>
+        </div>
+
+        <!-- Special Project Section -->
+        <div
+          class="relative p-6 transition-transform transform bg-white rounded-lg shadow-lg hover:scale-105"
+        >
+          <img
+            src="https://tsue.uz/media/news/photo_2024-10-24_19-11-34.jpg"
+            alt="Special project"
+            class="object-cover w-full h-40 rounded-lg"
+          />
+          <div
+            class="absolute inset-0 flex items-center justify-center text-white bg-opacity-75 rounded-lg bg-primary-blue"
+          >
+            <div class="text-center">
+              <h2 class="text-xl font-bold">Спецпроект</h2>
+              <p class="mt-1 text-sm">
+                О проекте на тему развития сельского хозяйства
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const articles = ref([
+  {
+    id: 1,
+    image: 'https://tsue.uz/media/news/photo_2024-10-24_19-11-34.jpg',
+    date: '30.10.2024',
+    category: 'Международное',
+    title:
+      'ТГУ представил вузам КНР форматы сотрудничества в сфере медиаобразования ТГУ представил вузам КНР форматы сотрудничества в сфере медиаобразования',
+  },
+  {
+    id: 2,
+    image: 'https://tsue.uz/media/news/photo_2024-10-24_19-11-34.jpg',
+    date: '29.10.2024',
+    category: 'Образование',
+    title:
+      'В ТГУ утвердили политику использования ИИ в образовательном процессе',
+  },
+])
+
+const getCategoryColor = (category: string) => {
+  switch (category) {
+    case 'Международное':
+      return 'bg-primary-blue'
+    case 'Образование':
+      return 'bg-green-600'
+    default:
+      return 'bg-gray-600'
+  }
+}
+</script>
+
+<style scoped>
+.small-card__img {
+  transition: height 0.5s ease;
+}
+.small-card:hover .small-card__img {
+  flex-shrink: 0;
+  will-change: height;
+  height: 0;
+}
+.small-card:hover .small-card__content {
+  flex-shrink: 1;
+}
+
+.small-card:hover .small-card__text {
+  flex-shrink: 1;
+  height: auto;
+  opacity: 1;
+}
+</style>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -2,10 +2,12 @@
   <MainHero />
   <DepartmentList />
   <InteractiveServices />
+  <AppNews />
 </template>
 
 <script setup lang="ts">
 import MainHero from '../components/MainHero.vue'
 import DepartmentList from '../components/DepartmentList.vue'
 import InteractiveServices from '../components/InteractiveServices.vue'
+import AppNews from '../components/AppNews.vue'
 </script>
