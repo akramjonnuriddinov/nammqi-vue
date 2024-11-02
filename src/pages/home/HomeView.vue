b99e47a9f9bc05e4c8db4bdd0d6b7ab596683376
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-11-02 10:10:00 +0500

    Fix folder url

--- src/pages/home/HomeView.vue
+++ src/pages/home/HomeView.vue
@@ -8,10 +8,10 @@
 </template>
 
 <script setup lang="ts">
-import MainHero from '../components/MainHero.vue'
-import DepartmentList from '../components/DepartmentList.vue'
-import InteractiveServices from '../components/InteractiveServices.vue'
-import AppNews from '../components/AppNews.vue'
-import AppAnnouncement from '../components/AppAnnouncement.vue'
-import AppStats from '../components/AppStats.vue'
+import MainHero from '@/components/home/MainHero.vue'
+import DepartmentList from '@/components/home/DepartmentList.vue'
+import InteractiveServices from '@/components/home/InteractiveServices.vue'
+import AppNews from '@/components/home/AppNews.vue'
+import AppAnnouncement from '@/components/home/AppAnnouncement.vue'
+import AppStats from '@/components/home/AppStats.vue'
 </script>
