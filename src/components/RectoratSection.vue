7c49fd470921697aac8ab92e64bd94bf5732f3ef
abdumajidovismoilbek007 <abdumajidovismoilbek007@gmail.com>
2024-11-21 10:42:22 +0500

    add news detail page by ismoil

--- src/components/RectoratSection.vue
+++ src/components/RectoratSection.vue
@@ -2,6 +2,11 @@
   <section class="py-10 bg-blue-50">
     <div class="container">
       <h2 class="mb-4 text-4xl font-semibold text-gray-800">Rektorat</h2>
+      <div class="flex justify-center  gap-[30px] mb-[30px]">
+        <div class="md:max-w-[400px] max-w-full w-full">
+          <ThreeDCard ></ThreeDCard>
+        </div>
+      </div>
       <div
         class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-[30px]"
       >
