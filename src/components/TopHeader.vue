6c81636556a43ca086af984617921951dd28a7b6
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-11-25 13:04:36 +0500

    add accessibility function

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import {
   Squares2X2Icon,
   MagnifyingGlassIcon,
   LanguageIcon,
 } from '@heroicons/vue/24/outline'
 import { EyeSlashIcon } from '@heroicons/vue/20/solid'
+import AppAccessibility from '@/components/AppAccessibility.vue'
+
+const accessibility = ref(false)
 </script>
 
 <template>
+  <AppAccessibility @click="accessibility = false" v-if="accessibility" />
   <div class="py-4 text-white bg-primary-blue">
     <div class="container flex items-center justify-between">
       <button class="flex items-center text-sm font-extrabold">
@@ -16,7 +21,7 @@ import { EyeSlashIcon } from '@heroicons/vue/20/solid'
       </button>
       <div class="flex items-center">
         <RouterLink class="mr-6" to="/admin">Admin</RouterLink>
-        <button>
+        <button @click="accessibility = true">
           <EyeSlashIcon class="w-6 mr-6" />
         </button>
         <button>
