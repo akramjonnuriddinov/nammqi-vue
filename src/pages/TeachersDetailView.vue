eb0a3f970c9be34700378d56f9113088e17bfa8d
abdumajidovismoilbek007 <abdumajidovismoilbek007@gmail.com>
2024-11-26 09:55:10 +0500

    add rektorat detail by ismoil

--- src/pages/TeachersDetailView.vue
+++ src/pages/TeachersDetailView.vue
@@ -1,116 +1,250 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ChevronRightIcon } from '@heroicons/vue/24/outline'
+</script>
 
 <template>
-  <div class="flex gap-10 container pb-8">
-    <div class="h-screen min-w-[400px] flex flex-col px-8">
-      <div>
-        <img
-          class="w-full"
-          src="https://my.nammqi.uz/storage/news/rektor.jpg"
-          alt=""
-        />
+  <div class="container bg-primary-gray py-10">
+    <h1 class="text-4xl font-semibold mb-10">Rektorat</h1>
+    <div class="flex gap-10 pb-8">
+      <div class="max-w-[470px]  flex flex-col                        ">
+        <img class="aspect-[5/3] object-cover sticky top-10 object-top" src="https://my.nammqi.uz/storage/news/sar.jpg" alt="" />
       </div>
-      <div class="text-center mt-5">
-        <h1 class="text-3xl font-bold text-gray-800">
-          Ergashev Sharibboy To'lanovich
-        </h1>
-        <p class="text-primary-blue">Rektor</p>
+      <div class="flex flex-col gap-10">
+        <div class="bg-white border p-16 px-12">
+          <h1 class="text-4xl font-bold text-gray-800">
+            Ergashev Sharibboy To'lanovich
+          </h1>
+          <p class="text-primary-blue mt-6">Rektor</p>
+        </div>
+        <div>
+          <div class="bg-white border p-16 flex flex-col gap-6">
+            <h3 class="text-xl m-auto font-bold border-b-2 border-primary-blue pb-1 w-max">Biografiya</h3>
+            <p class="indent-5">
+              1968-yilda tug'ilgan, Yoshlar masalalari va ma'naviy-ma'rifiy ishlar bo'yicha birinchi prorektor, Professor,
+              texnika fanlari doktori.
+            </p>
+            <ul class="list-[square] flex flex-col gap-5 ps-5 marker:text-primary-blue indent-3">
+              <li>
+                Toshkent to'qimachilik va yengil sanoat instituti
+                Namangan filiali assistenti 1985-1989 yy. - Toshkent to'qimachilik va
+                yengil sanoat instituti paxtani dastlabki ishlash kafedrasi aspiranti (1982-1985 yy.)
+              </li>
+              <li>
+                Namangan muhandislik-iqtisodiyot instituti
+                to'qimachilik mashinalari kafedrasi katta o'qituvchisi, dekan muovini,
+                dotsent (1989-1994 yy.)
+              </li>
+              <li>
+                Namangan muhandislik-iqtisodiyot instituti
+                ma'naviy-ma'rifiy ishlari bo'yicha birinchi prorektori (1994-1997 yy.)
+              </li>
+              <li>
+                Pop tumani hokimligining ijtimoiy masalalari bo'yicha o'rinbosar (1997-1998 yy.)
+              </li>
+              <li>
+                Namangan viloyati o'rta maxsus kasb-hunar ta'limi
+                boshqarmasi boshlig'i (1998-2000 yy.)
+              </li>
+              <li>
+                O'rta maxsus kasb-hunar ta'limi
+                rivojlantirish instituti dekani (2000-2001 yy.)
+              </li>
+              <li>
+                O'quvchilarni kasb-hunarga yo'naltirish va psixologik-pedagogik Respublika tashxis
+                markazi direktorining birinchi o'rinbosari (2001-2004 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Halq ta'limi vazirligi kadrlar va pedagog xodimlar
+                malkasini oshirish boshqarmasi boshlig'i o'rinbosari (2004-2005 yy.)
+              </li>
+              <li>
+                O'quvchilarni kasb-hunarga yo'naltirish va psixologik-pedagogik
+                Respublika tashxis markazi direktori (2005-2008 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Halq ta'limi vazirligi umumta'lim maktablarida
+                o'quv-tarbiya jarayonini rivojlantirish va tashkil etish bosh
+                boshqarmasi boshlig'i (2008-2009 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Halq ta'limi vazirligi huzuridagi multmedia umumta'lim dasturlarini
+                rivojlantirish markazi direktori (2009-2015 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Halq ta'limi vazirligi umumta'lim maktablarida
+                o'quv-tarbiya jarayonini rivojlantirish bosh boshqarmasi boshlig'i (2015-2016 yy.)
+              </li>
+              <li>
+                O'quvchilarni kasb-hunarga yo'naltirish va
+                psixologik-pedagogik Respublika tashxis markazi direktori (2016-2017 yy.)
+              </li>
+              <li>
+                O'rta maxsus kasb-hunar ta'limi muassasalarini axborot-metodik
+                ta'minlash xizmati boshlig'i (2017-2018 yy.)
+              </li>
+              <li>
+                Kasb-hunar ta'limi tizimini innovatsion rivojlantirish, pedagog kadrlarning malakasini
+                oshirish va ularni qayta tayyorlash instituti direktorining kasb-hunar
+                ta'limini innovatsion rivojlantirish bo'yicha o'rinbosari (2018-2019 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Oliy va o'rta maxsus ta'lim vazirligi
+                kasb-hunar ta'limini o'quv -metodik muvofiqlashtirish bosh boshqarmasi
+                boshlig'i (2019-2020 yy.)
+              </li>
+              <li>
+                Namangan muhandislik-qurilish instituti
+                rektori (2020 y. - h.v.)
+              </li>
+            </ul>
+            <div class="grid grid-cols-2 mt-5 justify-between">
+              <div class="text-xl">
+                <span class="text-primary-blue">F.I.O:</span>
+                <span class="ms-2">Ergashev Sharibboy To'lanovich</span>
+              </div>
+              <div class="text-xl">
+                <span class="text-primary-blue">Darajasi:</span>
+                <span class="ms-2">Yoshlar masalalari va ma'naviy-ma'rifiy ishlar bo'yicha birinchi
+                  prorektor, Professor, texnika fanlari doktori</span>
+              </div>
+              <div class="text-xl">
+                <span class="text-primary-blue">Telefon:</span>
+                <span class="ms-2">(69)234-15-23</span>
+              </div>
+              <div class="text-xl">
+                <span class="text-primary-blue">Email:</span>
+                <span class="ms-2">nammqi_info@edu.uz</span>
+              </div>
+            </div>
+          </div>
+          <div
+            class="bg-primary-blue flex justify-between p-6 border-x border-b group hover:bg-white transition-all duration-300 hover:text-black text-white">
+            <h1 class="font-semibold">Rektorga murojat</h1>
+            <ChevronRightIcon class="w-5 group-hover:translate-x-3 transition-transform duration-300"></ChevronRightIcon>
+          </div>
+        </div>
+
       </div>
     </div>
-    <div class="flex flex-col gap-10">
-      <div>
-        <h3 class="text-xl font-bold">Biografiya</h3>
-        <p class="text-lg font-medium text-center">MEHNAT FAOLIYATI</p>
-        <p>
-          1982-1985 yy. - Toshkent to‘qimachilik va yengil sanoat instituti
-          Namangan filiali assistenti 1985-1989 yy. - Toshkent to‘qimachilik va
-          yengil sanoat instituti paxtani dastlabki ishlash kafedrasi aspiranti
-          1989-1994 yy. – Namangan muhandislik-iqtisodiyot instituti
-          to‘qimachilik mashinalari kafedrasi katta o‘qituvchisi, dekan muovini,
-          dotsent 1994-1997 yy. - Namangan muhandislik-iqtisodiyot instituti
-          ma'naviy-ma'rifiy ishlari bo‘yicha birinchi prorektori 1997-1998 yy. –
-          Pop tumani hokimligining ijtimoiy masalalari bo‘yicha o‘rinbosar
-          1998-2000 yy. – Namangan viloyati o‘rta maxsus kasb-hunar ta'limi
-          boshqarmasi boshlig‘i 2000-2001 yy. – O‘rta maxsus kasb-hunar ta'limi
-          rivojlantirish instituti dekani 2001-2004 yy. – O‘quvchilarni
-          kasb-hunarga yo‘naltirish va psixologik-pedagogik Respublika tashxis
-          markazi direktorining birinchi o‘rinbosari 2004-2005 yy. – O‘zbekiston
-          Respublikasi Halq ta'limi vazirligi kadrlar va pedagog xodimlar
-          malkasini oshirish boshqarmasi boshlig‘i o‘rinbosari 2005-2008 yy. -
-          O‘quvchilarni kasb-hunarga yo‘naltirish va psixologik-pedagogik
-          Respublika tashxis markazi direktori 2008-2009 yy. - O‘zbekiston
-          Respublikasi Halq ta'limi vazirligi umumta'lim maktablarida
-          o‘quv-tarbiya jarayonini rivojlantirish va tashkil etish bosh
-          boshqarmasi boshlig‘i 2009-2015 yy. - O‘zbekiston Respublikasi Halq
-          ta'limi vazirligi huzuridagi multmedia umumta'lim dasturlarini
-          rivojlantirish markazi direktori 2015-2016 yy. - O‘zbekiston
-          Respublikasi Halq ta'limi vazirligi umumta'lim maktablarida
-          o‘quv-tarbiya jarayonini rivojlantirish bosh boshqarmasi boshlig‘i
-          2016-2017 yy - O‘quvchilarni kasb-hunarga yo‘naltirish va
-          psixologik-pedagogik Respublika tashxis markazi direktori 2017-2018
-          yy. - O‘rta maxsus kasb-hunar ta'limi muassasalarini axborot-metodik
-          ta'minlash xizmati boshlig‘i 2018-2019 yy - Kasb-hunar ta'limi
-          tizimini innovatsion rivojlantirish, pedagog kadrlarning malakasini
-          oshirish va ularni qayta tayyorlash instituti direktorining kasb-hunar
-          ta'limini innovatsion rivojlantirish bo‘yicha o‘rinbosari 2019-2020
-          yy. – O‘zbekiston Respublikasi Oliy va o‘rta maxsus ta'lim vazirligi
-          kasb-hunar ta'limini o‘quv -metodik muvofiqlashtirish bosh boshqarmasi
-          boshlig‘i 2020 y. - h.v. – Namangan muhandislik-qurilish instituti
-          rektori
-        </p>
-        <div class="grid grid-cols-2 mt-5 justify-between">
-          <div class="text-xl">
-            <span class="text-primary-blue">F.I.O:</span>
-            <span class="ms-2">Ergashev Sharibboy To'lanovich</span>
-          </div>
-          <div class="text-xl">
-            <span class="text-primary-blue">Darajasi:</span>
-            <span class="ms-2"
-              >Yoshlar masalalari va ma’naviy-ma’rifiy ishlar bo‘yicha birinchi
-              prorektor, Professor, texnika fanlari doktori</span
-            >
-          </div>
-          <div class="text-xl">
-            <span class="text-primary-blue">Telefon:</span>
-            <span class="ms-2">(69)234-15-23</span>
-          </div>
-          <div class="text-xl">
-            <span class="text-primary-blue">Email:</span>
-            <span class="ms-2">nammqi_info@edu.uz</span>
-          </div>
-        </div>
+    <div class="flex gap-10 pb-8">
+      <div class="max-w-[470px]  flex flex-col">
+        <img class="aspect-[5/3] object-cover sticky top-10 object-top" src="https://my.nammqi.uz/storage/news/sar.jpg" alt="" />
       </div>
-
-      <div>
-        <h3 class="text-xl font-bold mb-10">Portfolio</h3>
-        <div
-          class="grid grid-cols-1 md:grid-cols-2 gap-5 xl:grid-cols-3 2xl:grid-cols-4"
-        >
-          <div>
-            <img src="https://my.nammqi.uz/storage/news/rektor.jpg" alt="" />
+      <div class="flex flex-col gap-10">
+        <div class="bg-white border p-16 px-12">
+          <h1 class="text-4xl font-bold text-gray-800">
+            Ergashev Sharibboy To'lanovich
+          </h1>
+          <p class="text-primary-blue mt-6">Rektor</p>
+        </div>
+        <div>
+          <div class="bg-white border p-16 flex flex-col gap-6">
+            <h3 class="text-xl m-auto font-bold border-b-2 border-primary-blue pb-1 w-max">Biografiya</h3>
+            <p class="indent-5">
+              1968-yilda tug'ilgan, Yoshlar masalalari va ma'naviy-ma'rifiy ishlar bo'yicha birinchi prorektor, Professor,
+              texnika fanlari doktori.
+            </p>
+            <ul class="list-[square] flex flex-col gap-5 ps-5 marker:text-primary-blue indent-3">
+              <li>
+                Toshkent to'qimachilik va yengil sanoat instituti
+                Namangan filiali assistenti 1985-1989 yy. - Toshkent to'qimachilik va
+                yengil sanoat instituti paxtani dastlabki ishlash kafedrasi aspiranti (1982-1985 yy.)
+              </li>
+              <li>
+                Namangan muhandislik-iqtisodiyot instituti
+                to'qimachilik mashinalari kafedrasi katta o'qituvchisi, dekan muovini,
+                dotsent (1989-1994 yy.)
+              </li>
+              <li>
+                Namangan muhandislik-iqtisodiyot instituti
+                ma'naviy-ma'rifiy ishlari bo'yicha birinchi prorektori (1994-1997 yy.)
+              </li>
+              <li>
+                Pop tumani hokimligining ijtimoiy masalalari bo'yicha o'rinbosar (1997-1998 yy.)
+              </li>
+              <li>
+                Namangan viloyati o'rta maxsus kasb-hunar ta'limi
+                boshqarmasi boshlig'i (1998-2000 yy.)
+              </li>
+              <li>
+                O'rta maxsus kasb-hunar ta'limi
+                rivojlantirish instituti dekani (2000-2001 yy.)
+              </li>
+              <li>
+                O'quvchilarni kasb-hunarga yo'naltirish va psixologik-pedagogik Respublika tashxis
+                markazi direktorining birinchi o'rinbosari (2001-2004 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Halq ta'limi vazirligi kadrlar va pedagog xodimlar
+                malkasini oshirish boshqarmasi boshlig'i o'rinbosari (2004-2005 yy.)
+              </li>
+              <li>
+                O'quvchilarni kasb-hunarga yo'naltirish va psixologik-pedagogik
+                Respublika tashxis markazi direktori (2005-2008 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Halq ta'limi vazirligi umumta'lim maktablarida
+                o'quv-tarbiya jarayonini rivojlantirish va tashkil etish bosh
+                boshqarmasi boshlig'i (2008-2009 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Halq ta'limi vazirligi huzuridagi multmedia umumta'lim dasturlarini
+                rivojlantirish markazi direktori (2009-2015 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Halq ta'limi vazirligi umumta'lim maktablarida
+                o'quv-tarbiya jarayonini rivojlantirish bosh boshqarmasi boshlig'i (2015-2016 yy.)
+              </li>
+              <li>
+                O'quvchilarni kasb-hunarga yo'naltirish va
+                psixologik-pedagogik Respublika tashxis markazi direktori (2016-2017 yy.)
+              </li>
+              <li>
+                O'rta maxsus kasb-hunar ta'limi muassasalarini axborot-metodik
+                ta'minlash xizmati boshlig'i (2017-2018 yy.)
+              </li>
+              <li>
+                Kasb-hunar ta'limi tizimini innovatsion rivojlantirish, pedagog kadrlarning malakasini
+                oshirish va ularni qayta tayyorlash instituti direktorining kasb-hunar
+                ta'limini innovatsion rivojlantirish bo'yicha o'rinbosari (2018-2019 yy.)
+              </li>
+              <li>
+                O'zbekiston Respublikasi Oliy va o'rta maxsus ta'lim vazirligi
+                kasb-hunar ta'limini o'quv -metodik muvofiqlashtirish bosh boshqarmasi
+                boshlig'i (2019-2020 yy.)
+              </li>
+              <li>
+                Namangan muhandislik-qurilish instituti
+                rektori (2020 y. - h.v.)
+              </li>
+            </ul>
+            <div class="grid grid-cols-2 mt-5 justify-between">
+              <div class="text-xl">
+                <span class="text-primary-blue">F.I.O:</span>
+                <span class="ms-2">Ergashev Sharibboy To'lanovich</span>
+              </div>
+              <div class="text-xl">
+                <span class="text-primary-blue">Darajasi:</span>
+                <span class="ms-2">Yoshlar masalalari va ma'naviy-ma'rifiy ishlar bo'yicha birinchi
+                  prorektor, Professor, texnika fanlari doktori</span>
+              </div>
+              <div class="text-xl">
+                <span class="text-primary-blue">Telefon:</span>
+                <span class="ms-2">(69)234-15-23</span>
+              </div>
+              <div class="text-xl">
+                <span class="text-primary-blue">Email:</span>
+                <span class="ms-2">nammqi_info@edu.uz</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <img src="https://my.nammqi.uz/storage/news/rektor.jpg" alt="" />
-          </div>
-          <div>
-            <img src="https://my.nammqi.uz/storage/news/rektor.jpg" alt="" />
-          </div>
-          <div>
-            <img src="https://my.nammqi.uz/storage/news/rektor.jpg" alt="" />
-          </div>
-          <div>
-            <img src="https://my.nammqi.uz/storage/news/rektor.jpg" alt="" />
-          </div>
-          <div>
-            <img src="https://my.nammqi.uz/storage/news/rektor.jpg" alt="" />
-          </div>
-          <div>
-            <img src="https://my.nammqi.uz/storage/news/rektor.jpg" alt="" />
-          </div>
-          <div>
-            <img src="https://my.nammqi.uz/storage/news/rektor.jpg" alt="" />
+          <div
+            class="bg-primary-blue flex justify-between p-6 border-x border-b group hover:bg-white transition-all duration-300 hover:text-black text-white">
+            <h1 class="font-semibold">Rektorga murojat</h1>
+            <ChevronRightIcon class="w-5 group-hover:translate-x-3 transition-transform duration-300"></ChevronRightIcon>
           </div>
         </div>
+
       </div>
     </div>
   </div>
