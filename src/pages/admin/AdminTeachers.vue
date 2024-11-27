f6024fcb0d2e7cd7d3ce82019d5e3d50bf4b7686
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-11-27 10:53:32 +0500

    add admin teachers component

---
+++ src/pages/admin/AdminTeachers.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import AppEditor from '@/components/shared/AppEditor.vue'
+import { ref } from 'vue'
+
+interface Teacher {
+  id: number
+  fullname: string
+  content: string
+}
+
+const editorContent = ref<string>('<p>Initial value</p>')
+const teacher = ref<Teacher>({
+  id: 0,
+  fullname: '',
+  content: '',
+})
+
+const handleInput = (content: string) => {
+  editorContent.value = content
+  teacher.value.content = content
+}
+
+const postData = () => {
+  console.log('TEACHER: ', teacher.value)
+  teacher.value.id++
+}
+</script>
+
+<template>
+  <h2>Create Teachers</h2>
+  <input v-model="teacher.fullname" type="text" />
+  <AppEditor
+    :value="editorContent"
+    :edit-editor="editorContent"
+    @input="handleInput"
+  />
+  <button @click="postData">Click</button>
+</template>
