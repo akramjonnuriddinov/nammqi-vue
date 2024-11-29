17aa3966346c606f6f931c81d6311cffadef212b
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-11-29 16:29:06 +0500

    fix  nav links

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,24 +10,24 @@ export const navCategories: NavCategory[] = [
     columns: [
       {
         links: [
-          { title: 'Institut Ustavi', href: '#' },
-          { title: 'Institut haqida', href: '#' },
-          { title: 'Rahbariyat', href: '#' },
-          { title: 'Reytinglar', href: '#' },
+          { title: 'Institut Ustavi', href: '/intitute-charter' },
+          { title: 'Institut haqida', href: '/intitute' },
+          { title: 'Rahbariyat', href: '/rectorat' },
+          { title: 'Rekvizitlar', href: '/props' },
         ],
       },
       {
         links: [
-          { title: 'Fakultetlar', href: '#' },
-          { title: 'Kafedralar', href: '#' },
-          { title: "Bo'limlar", href: '#' },
+          { title: 'Fakultetlar', href: '/faculties' },
+          { title: 'Kafedralar', href: '/departments' },
+          { title: "Bo'limlar", href: '/branches' },
           { title: "O'qituvchilar", href: '/teachers' },
         ],
       },
     ],
   },
   {
-    title: 'Taʼlim',
+    title: 'Qabul',
     isInnerLinks: true,
     path: '',
     description:
@@ -35,133 +35,13 @@ export const navCategories: NavCategory[] = [
     columns: [
       {
         links: [
-          { title: 'Kurs katalogi', href: '#' },
-          { title: 'Bakalavriat', href: '#' },
-          { title: 'Malaka talablari', href: '#' },
-          { title: 'O‘quv dasturlari Sillabuslar)', href: '#' },
+          { title: 'Qabul 2024', href: '/qabul-2024' },
+          { title: 'Bakalavr', href: '/bachelor' }
         ],
       },
       {
         links: [
-          { title: 'Resurslar', href: '#' },
-          { title: 'Magistratura', href: '#' },
-          { title: 'O‘quv rejalar', href: '#' },
-          { title: 'Masofaviy o‘qitish tizimi', href: '#' },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Ilm-fan',
-    isInnerLinks: true,
-    path: '',
-    description:
-      'XX1 asr boshlarida Markaziy Osiyo hududida - O‘zbekistonda uchta tijorat bilim yurti savdo-sanoat firmalari va banklar uchun mutaxassislar tayyorlab bergan. Rasman oliy maqomli iqtisodchilarni tayyorlash 1918-yil 12-may boshlangan.',
-    columns: [
-      {
-        links: [
-          { title: 'Seminarlar', href: '#' },
-          { title: 'Ilmiy jurnallar', href: '#' },
-          { title: 'Kutilayotgan konferensiyalar', href: '#' },
-          { title: 'Sertifikatlar', href: '#' },
-        ],
-      },
-      {
-        links: [
-          { title: 'Ilmiy maqolalar', href: '#' },
-          { title: 'Konferensiyalar', href: '#' },
-          { title: 'Ilmiy kengashlar', href: '#' },
-          { title: 'Ilmiy markazlar', href: '#' },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Xalqarolashtirish',
-    isInnerLinks: true,
-    path: '',
-    description:
-      'XX1 asr boshlarida Markaziy Osiyo hududida - O‘zbekistonda uchta tijorat bilim yurti savdo-sanoat firmalari va banklar uchun mutaxassislar tayyorlab bergan. Rasman oliy maqomli iqtisodchilarni tayyorlash 1918-yil 12-may boshlangan.',
-    columns: [
-      {
-        links: [
-          { title: 'Xalqaro aloqalar departamenti xodimlari', href: '#' },
-          { title: 'Xalqaro grantlar', href: '#' },
-          { title: 'Xalqaro konferensiyalar', href: '#' },
-          {
-            title: 'Xorijlik pedagoglar uchun malaka oshirish dasturlari',
-            href: '#',
-          },
-        ],
-      },
-      {
-        links: [
-          { title: 'Xalqaro hamkorlik aloqalari', href: '#' },
-          { title: 'Xalqaro ilmiy aloqalar', href: '#' },
-          { title: 'Xorijda malaka oshirish va taʼlim', href: '#' },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Talabalar hayoti',
-    isInnerLinks: true,
-    path: '',
-    description:
-      'XX1 asr boshlarida Markaziy Osiyo hududida - O‘zbekistonda uchta tijorat bilim yurti savdo-sanoat firmalari va banklar uchun mutaxassislar tayyorlab bergan. Rasman oliy maqomli iqtisodchilarni tayyorlash 1918-yil 12-may boshlangan.',
-    columns: [
-      {
-        links: [
-          { title: 'Jamoaviy klublar', href: '#' },
-          { title: 'Ijtimoiy hayot', href: '#' },
-          { title: 'Tanlovlar', href: '#' },
-          { title: 'Talabalar turar joyi', href: '#' },
-          { title: 'Kafeteriyalar', href: '#' },
-          { title: 'Imkoniyati cheklanganlar uchun qulayliklar', href: '#' },
-        ],
-      },
-      {
-        links: [
-          { title: 'Sog‘liqni saqlashni xizmati', href: '#' },
-          { title: 'Ijtimoiy xonalar', href: '#' },
-          { title: 'Ijtimoiy muhofaza markazi', href: '#' },
-          { title: 'Sport inshootlari', href: '#' },
-          { title: 'Kitob do‘koni', href: '#' },
-          { title: 'Talaba fikri', href: '#' },
-        ],
-      },
-    ],
-  },
-  {
-    title: 'Qabul-2024',
-    isInnerLinks: true,
-    path: '',
-    description:
-      'XX1 asr boshlarida Markaziy Osiyo hududida - O‘zbekistonda uchta tijorat bilim yurti savdo-sanoat firmalari va banklar uchun mutaxassislar tayyorlab bergan. Rasman oliy maqomli iqtisodchilarni tayyorlash 1918-yil 12-may boshlangan.',
-    columns: [
-      {
-        links: [
-          { title: 'Ikkinchi taʼlim to‘g‘risidagi nizom', href: '#' },
-          {
-            title:
-              '2024/2025 o‘quv yili uchun ikkinchi va undan keyingi oliy maʼlumot olish bo‘yicha qabul shartlari',
-            href: '#',
-          },
-          { title: 'Mahalliy abiturentlarga', href: '#' },
-          { title: 'Qabul kvotalari', href: '#' },
-          { title: 'O‘qishni ko‘chirish bo‘yicha maʼlumotlar', href: '#' },
-          { title: 'Javoblar varaqasi bilan ishlash', href: '#' },
-          { title: 'Tez-tez so‘raladigan savollarga javoblar', href: '#' },
-        ],
-      },
-      {
-        links: [
-          { title: 'Qabul 2024', href: '#' },
-          { title: 'Xalqaro abituriyentlarga', href: '#' },
-          { title: 'Qabul bo‘yicha bog‘lanish', href: '#' },
-          { title: 'Abituriyentlar uchun yo‘riqnoma', href: '#' },
-          { title: 'Bakalavriatga qabul uchun ro‘yhatdan o‘tish', href: '#' },
-          { title: 'Texnikum bitiruvchilari qabuli-2024', href: '#' },
+          { title: 'Magistratura', href: '/masters-degree' },
         ],
       },
     ],
@@ -175,15 +55,15 @@ export const navCategories: NavCategory[] = [
     columns: [
       {
         links: [
-          { title: 'Yangiliklar', href: '#' },
-          { title: 'Kutilayotgan tadbirlar', href: '#' },
-          { title: 'Kontaktlar', href: '#' },
+          { title: 'Yangiliklar', href: '/news' },
+          { title: "E'lonlar", href: '/announcements' },
+          { title: 'Tadbirlar', href: '/events' },
         ],
       },
       {
         links: [
-          { title: 'Videogalereya', href: '#' },
-          { title: 'Fotogalareya', href: '#' },
+          { title: 'Videogalereya', href: '/video-gallery' },
+          { title: 'Fotogalareya', href: '/photo-gallery' },
         ],
       },
     ],
