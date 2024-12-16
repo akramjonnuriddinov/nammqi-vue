2618ba8af1e81e1b21ec60e0a1a5d77f980df448
akramjonnuriddinov <nn.akramjon@gmail.com>
2024-12-16 11:15:23 +0500

    add faculties page

--- src/pages/default/FacultiesView.vue
+++ src/pages/default/FacultiesView.vue
@@ -1,57 +1,146 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import SearchIcon from '@/components/icons/SearchIcon.vue'
+import LocationIcon from '@/components/icons/LocationIcon.vue'
+import PhoneIcon from '@/components/icons/PhoneIcon.vue'
+import LinkIcon from '@/components/icons/LinkIcon.vue'
+import EmailIcon from '@/components/icons/EmailIcon.vue'
 
 interface Faculty {
   name: string
-  subFaculties: string[]
+  image: string
+  phone: string
+  email: string
+  link: string
+  location: string
 }
 
-const faculties = ref<Faculty[]>([
+const searchQuery = ref('')
+const faculties: Faculty[] = [
   {
-    name: 'Engineering',
-    subFaculties: ['Computer Science', 'Mechanical', 'Electrical'],
+    name: 'Iqtisodiyot va Boshqaruv fakulteti',
+    image: 'https://my.nammqi.uz/storage/photo-2022-09-22-17-14-48.jpg',
+    phone: '+998 71-000-00-00',
+    email: 'nammqi@gmail.com',
+    link: 'rektorat@nammqi.uz',
+    location: 'Namangan Viloyat Temiryo‘lchilar ko‘chasi 1-uy',
   },
-  { name: 'Arts', subFaculties: ['History', 'Literature', 'Philosophy'] },
-  { name: 'Science', subFaculties: ['Physics', 'Chemistry', 'Biology'] },
-])
+  {
+    name: 'Energetika va Mehnat muhozasi fakulteti',
+    image: 'https://my.nammqi.uz/storage/photo-2022-09-22-17-14-58.jpg',
+    phone: '+998 71-000-00-00',
+    email: 'nammqi@gmail.com',
+    link: 'rektorat@nammqi.uz',
+    location: 'Namangan Viloyat Temiryo‘lchilar ko‘chasi 1-uy',
+  },
+  {
+    name: 'Mashinasozlik fakulteti',
+    image: 'https://my.nammqi.uz/storage/photo-2022-09-22-17-14-37.jpg',
+    phone: '+998 71-000-00-00',
+    email: 'nammqi@gmail.com',
+    link: 'rektorat@nammqi.uz',
+    location: 'Namangan Viloyat Temiryo‘lchilar ko‘chasi 1-uy',
+  },
+  {
+    name: 'Transport fakulteti',
+    image: 'https://my.nammqi.uz/storage/photo-2022-09-22-17-14-52.jpg',
+    phone: '+998 71-000-00-00',
+    email: 'nammqi@gmail.com',
+    link: 'rektorat@nammqi.uz',
+    location: 'Namangan Viloyat Temiryo‘lchilar ko‘chasi 1-uy',
+  },
+  {
+    name: 'Muhandislik kommunikatsiyalari fakulteti',
+    image: 'https://my.nammqi.uz/storage/general/2023-03-29-165018.jpg',
+    phone: '+998 71-000-00-00',
+    email: 'nammqi@gmail.com',
+    link: 'rektorat@nammqi.uz',
+    location: 'Namangan Viloyat Temiryo‘lchilar ko‘chasi 1-uy',
+  },
+  {
+    name: 'Sanoatni Axborotlashtirish fakulteti',
+    image: 'https://my.nammqi.uz/storage/img-20231226-151252-748.jpg',
+    phone: '+998 71-000-00-00',
+    email: 'nammqi@gmail.com',
+    link: 'rektorat@nammqi.uz',
+    location: 'Namangan Viloyat Temiryo‘lchilar ko‘chasi 1-uy',
+  },
+  {
+    name: 'Qurilish fakulteti',
+    image: 'https://my.nammqi.uz/storage/photo-2022-09-22-17-14-48.jpg',
+    phone: '+998 71-000-00-00',
+    email: 'nammqi@gmail.com',
+    link: 'rektorat@nammqi.uz',
+    location: 'Namangan Viloyat Temiryo‘lchilar ko‘chasi 1-uy',
+  },
+]
 
-const selectedFaculty = ref<Faculty | null>(null)
-
-function selectFaculty(faculty: Faculty) {
-  selectedFaculty.value = faculty
-}
+const filteredFaculties = computed(() => {
+  const query = searchQuery.value.toLowerCase()
+  return faculties.filter((faculty) =>
+    faculty.name.toLowerCase().includes(query)
+  )
+})
 </script>
 
 <template>
-  <div class="container p-6 mx-auto">
-    <h1 class="mb-6 text-3xl font-bold">Faculties</h1>
-    <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <div
-        v-for="faculty in faculties"
-        :key="faculty.name"
-        class="p-4 bg-white rounded-lg shadow-lg"
-      >
-        <h2 class="mb-2 text-xl font-semibold">{{ faculty.name }}</h2>
-        <button
-          class="px-4 py-2 mt-2 text-white bg-blue-500 rounded hover:bg-blue-600"
-          @click="selectFaculty(faculty)"
-        >
-          View Sub-Faculties
-        </button>
+  <section class="min-h-screen py-10 bg-[#f6f6f6]">
+    <div class="container p-6 mx-auto">
+      <h1 class="mb-6 text-3xl font-bold">Fakultetlar</h1>
+      <div class="relative mb-6">
+        <input
+          v-model="searchQuery"
+          class="w-full px-20 py-4 border border-[#b4b4b4]"
+          placeholder="Fakultetni qidirish"
+          type="text"
+        />
+        <SearchIcon
+          class="absolute w-4 -translate-y-1/2 text-primary-blue left-4 top-1/2"
+        />
       </div>
-    </div>
-
-    <div v-if="selectedFaculty" class="mt-8">
-      <h2 class="text-2xl font-bold">{{ selectedFaculty.name }}</h2>
-      <ul class="mt-4 space-y-2">
+      <ul class="flex flex-col">
         <li
-          v-for="subFaculty in selectedFaculty.subFaculties"
-          :key="subFaculty"
-          class="p-2 bg-gray-100 rounded"
+          v-for="faculty in filteredFaculties"
+          :key="faculty.name"
+          class="flex items-center gap-10 px-10 pt-8 pb-6 mb-5 bg-white"
         >
-          {{ subFaculty }}
+          <div>
+            <img :src="faculty.image" alt="" class="w-[70px]" />
+          </div>
+          <RouterLink
+            to="/"
+            class="w-full text-xl font-semibold text-black max-w-80"
+            >{{ faculty.name }}</RouterLink
+          >
+          <div class="flex flex-col gap-5 ml-auto">
+            <a
+              href="#"
+              class="flex items-center gap-2 text-sm text-primary-blue"
+            >
+              <PhoneIcon class="w-4 mr-5 text-primary-blue" />
+              <span class="text-sm">{{ faculty.phone }}</span>
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-2 text-sm text-primary-blue"
+            >
+              <LinkIcon class="w-4 mr-5 text-primary-blue" />
+              <span class="text-sm">{{ faculty.link }}</span>
+            </a>
+            <a
+              href="#"
+              class="flex items-center gap-2 text-sm text-primary-blue"
+            >
+              <EmailIcon class="w-4 mr-5 text-primary-blue" />
+              <span class="text-sm">{{ faculty.email }}</span>
+            </a>
+          </div>
+          <div class="flex items-start gap-4 ml-auto">
+            <LocationIcon class="flex w-4 text-primary-blue" />
+            <span class="w-full text-sm max-w-60">{{ faculty.location }}</span>
+          </div>
         </li>
       </ul>
     </div>
-  </div>
+  </section>
 </template>
